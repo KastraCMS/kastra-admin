c8960facd625e4bea12b30ff15b4125bcbd0e72e
Babat <babat42@gmail.com>
2018-04-09 13:36:02 +0200

    Update test content

--- src/components/page/page.jsx
+++ src/components/page/page.jsx
@@ -46,18 +46,18 @@ export default class Page extends Component {
     }
 
     componentDidMount() {
-        //this.fetchSettings();
+        
         if(this.state.pageId === undefined) {
             return;
         }
 
         this.setState({ 
-            title: 'title test',
+            title: 'Page title test',
             keyname: 'titletest',
             templateId: '2',
-            metaKeywords: 'key1, key 2, key3',
-            metaDescription: 'dd  dhbiebhezbhebhe ibdhibeu zaujdd aezua inaihe !éçe',
-            metaRobot: 'robot index robot index'
+            metaKeywords: 'key1, key2, key3',
+            metaDescription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            metaRobot: 'noindex'
         });
     }
 
